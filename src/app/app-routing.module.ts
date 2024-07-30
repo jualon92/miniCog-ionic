42259f7252b7add6e8ab90e730aa656b70d93807
Juan Ignacio Londra <londrajuan@gmail.com>
2024-07-29 22:19:46 -0300

    fix routing refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,31 +4,31 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
    path: '',
-   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'three-word-registration',
-    loadChildren: () => import('./three-word-registration/three-word-registration.module').then( m => m.ThreeWordRegistrationPageModule)
+    loadChildren: () => import('./pages/three-word-registration/three-word-registration.module').then( m => m.ThreeWordRegistrationPageModule)
   }, 
   {
     path: 'before-we-start',
-    loadChildren: () => import('./before-we-start/before-we-start.module').then( m => m.BeforeWeStartPageModule)
+    loadChildren: () => import('./pages/before-we-start/before-we-start.module').then( m => m.BeforeWeStartPageModule)
   },
   {
     path: 'clow-drawing',
-    loadChildren: () => import('./clow-drawing/clow-drawing.module').then( m => m.ClowDrawingPageModule)
+    loadChildren: () => import('./pages/clow-drawing/clow-drawing.module').then( m => m.ClowDrawingPageModule)
   },
   {
     path: 'scoring',
-    loadChildren: () => import('./scoring/scoring.module').then( m => m.ScoringPageModule)
+    loadChildren: () => import('./pages/scoring/scoring.module').then( m => m.ScoringPageModule)
   },
   {
     path: 'score-clock',
-    loadChildren: () => import('./score-clock/score-clock.module').then( m => m.ScoreClockPageModule)
+    loadChildren: () => import('./pages/score-clock/score-clock.module').then( m => m.ScoreClockPageModule)
   },
   {
     path: 'results',
-    loadChildren: () => import('./results/results.module').then( m => m.ResultsPageModule)
+    loadChildren: () => import('./pages/results/results.module').then( m => m.ResultsPageModule)
   }
 ];
 @NgModule({
