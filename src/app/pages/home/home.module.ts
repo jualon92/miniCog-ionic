bc7790652fdba602f01a61280a45b5e1b94687b2
Juan Ignacio Londra <londrajuan@gmail.com>
2024-07-31 02:21:14 -0300

    ask for lang first via modal + dropdown

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -10,11 +10,17 @@ import { HomePage } from './home.page';
 import { TranslocoHttpLoader } from 'src/app/transloco-loader';
 import { TranslocoRootModule } from 'src/app/transloco-root.module';
 import { TranslocoModule } from '@jsverse/transloco';
-
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { ListboxModule } from 'primeng/listbox';
 @NgModule({
   imports: [
+    DropdownModule,
+    ListboxModule,
     CommonModule,
     FormsModule,
+    DialogModule,
     TranslocoRootModule,
     IonicModule,
     HomePageRoutingModule
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
+   displayLangOptions = true;
+   languages = [
+    { name: 'English', code: 'en' },
+    { name: 'Spanish', code: 'es' },
+    { name: 'French', code: 'fr' },
+    { name: 'German', code: 'de' },
+    { name: 'Italian', code: 'it' },
+];
+    
 
-  constructor() { }
 
-  ngOnInit() {
+   selectedLang: any | undefined;
+
+  constructor(private translocoService: TranslocoService) { 
   }
+
+ 
+
+  handleLangSelection(){
+      this.translocoService.setActiveLang(this.selectedLang.code);
+      this.displayLangOptions = false;
+  }
+
 
 }
