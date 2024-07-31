14393bb104d638a17c4d0b1fe48a90861ac92d8f
Juan Ignacio Londra <londrajuan@gmail.com>
2024-07-30 22:28:49 -0300

    add spanish translate files

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,11 +7,15 @@ import { IonButton, IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { TranslocoHttpLoader } from 'src/app/transloco-loader';
+import { TranslocoRootModule } from 'src/app/transloco-root.module';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    TranslocoRootModule,
     IonicModule,
     HomePageRoutingModule
   ],
