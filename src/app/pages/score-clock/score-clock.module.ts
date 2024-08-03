05d6b65adea7cb781452d926fe51f7780c553be4
Juan Ignacio Londra <londrajuan@gmail.com>
2024-08-02 22:28:51 -0300

    add scoring the clock translate

--- src/app/pages/score-clock/score-clock.module.ts
+++ src/app/pages/score-clock/score-clock.module.ts
@@ -9,12 +9,14 @@ import { ScoreClockPageRoutingModule } from './score-clock-routing.module';
 import { ScoreClockPage } from './score-clock.page';
 import { MessagesModule } from 'primeng/messages';
 import { ButtonModule } from 'primeng/button';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    TranslocoModule,
     MessagesModule,
     ButtonModule,
     ScoreClockPageRoutingModule
