71bd69fe02079b2862d41cf90955af7aa3b07f96
Juan Ignacio Londra <londrajuan@gmail.com>
2024-08-07 00:56:30 -0300

    home component  refactor

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,9 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { TranslocoService } from '@jsverse/transloco';
 import { Storage } from '@ionic/storage-angular';
 import { MessageService } from 'primeng/api';
@@ -8,64 +13,58 @@ import { MessageService } from 'primeng/api';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   providers: [Storage, MessageService],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage implements OnInit, AfterViewInit {
-   displayLangOptions = false;
-   NO_LANG_CACHE = true;
-    
+  displayLangOptions = false;
+  NO_LANG_CACHE = true;
 
-  constructor(private translocoService: TranslocoService,
+  constructor(
+    private translocoService: TranslocoService,
     private storage: Storage,
     private messageService: MessageService
-  ) { 
-    
-  }
+  ) {}
 
   async ngOnInit() {
     await this.storage.create();
     this.handleAppLang();
   }
-  
-  
-  ngAfterViewInit(){
+
+  ngAfterViewInit() {
     this.handleDisplayNotification();
   }
 
-
-  handleDisplayNotification(){
+  handleDisplayNotification() {
     const userWidth = window.innerWidth;
-    if(userWidth > 1023){
-      this.messageService.add({ severity: 'warn', summary: 'Info', detail: 'Recommended to view on mobile device' });
+    if (userWidth > 1023) {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Info',
+        detail: 'Recommended to view on mobile device',
+      });
     }
+  }
 
-  } 
-
-
-  async handleAppLang(){
-    if (this.NO_LANG_CACHE){
+  async handleAppLang() {
+    if (this.NO_LANG_CACHE) {
       await this.storage.clear();
       this.displayLangOptions = true;
       return;
     }
 
     const selectedLang = await this.storage.get('selectedLang');
-    if(selectedLang){
+    if (selectedLang) {
       this.translocoService.setActiveLang(selectedLang);
-      console.log("lang already active: ", selectedLang);
-    }else{
-      console.log("no active lang, showing modal");
+      console.log('lang already active: ', selectedLang);
+    } else {
+      console.log('no active lang, showing modal');
       this.displayLangOptions = true;
     }
   }
 
-  handleLangSelection(code: string){
-      this.storage.set('selectedLang', code);
-      this.translocoService.setActiveLang(code);
-      this.displayLangOptions = false;
+  handleLangSelection(code: string) {
+    this.storage.set('selectedLang', code);
+    this.translocoService.setActiveLang(code);
+    this.displayLangOptions = false;
   }
-
-  
-
-
 }
