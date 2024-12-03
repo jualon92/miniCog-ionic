acad1ef0ea1b97c6db7d1844749a89f3c0235b11
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-03 00:53:35 -0300

    grab scoring data sets from indexed db

--- src/app/pages/scoring/scoring.page.ts
+++ src/app/pages/scoring/scoring.page.ts
@@ -1,14 +1,31 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/storage/storage.service';
 
 @Component({
   selector: 'app-scoring',
   templateUrl: './scoring.page.html',
-  styleUrls: ['./scoring.page.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./scoring.page.scss'], 
+  providers: [StorageService],
 })
-export class ScoringPage  {
+export class ScoringPage implements OnInit  {
+  timesVisited: number = 0;
+  constructor(
+    private storage: StorageService,
+  ) { }
 
-  constructor() { }
+ 
 
+  async ngOnInit() {
+    //TODO: replace in favor of resolvers
+    setTimeout(async () => {
+      this.timesVisited = await this.storage.getTimesDone();
+ 
+    }, 100);
+      
+ 
+     
+    
+  }
 
+ 
 }
