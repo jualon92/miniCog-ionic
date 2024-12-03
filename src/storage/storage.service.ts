9061de4866a664fb67ee2ffc54feb97f83187e33
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-03 10:01:57 -0300

    feat: saving points

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Storage } from '@ionic/storage-angular';
-import { TIMES_DONE } from './storage.entities';
+import { CLOCK_KEY, ClockPoints, HISTORY, TIMES_DONE, WORD_KEY } from './storage.entities';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +25,44 @@ export class StorageService {
 
   setTimesDone(timesDone: number){
     this._storage?.set(TIMES_DONE, timesDone);
+  }
+
+  getWordPoints(){
+    return this._storage?.get(WORD_KEY);
+  }
+
+  setClockPoints(clockPoints:  ClockPoints){
+    this._storage?.set(CLOCK_KEY, clockPoints);
+  }
+
+  getClockPoints(): Promise<ClockPoints> | undefined{
+    return this._storage?.get(CLOCK_KEY);
+  }
+  setWordPoints(wordPoints: number){
+    this._storage?.set(WORD_KEY, wordPoints);
+  }
+
+  async saveState(){
+    
+    const state = { 
+      wordPoints: await this.getWordPoints(),
+      clockPoints: await this.getClockPoints()
+    }
+    this.addToArray(HISTORY, state);
+  }
+
+  async addToArray(key: string, element: any) {
+    let currentArray = await this._storage?.get(key) || [];
+    if (!Array.isArray(currentArray)) {
+      currentArray = [];
+    }
+    currentArray.push(element);
+    await this._storage?.set(key, currentArray);
+  }
+
+
+  getHistory(){
+    return this._storage?.get(HISTORY);
   }
   // Create and expose methods that users of this service can
   // call, for example:
