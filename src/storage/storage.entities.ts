5cc38abf0be4dbd216bbeecdd346a17e738c15e8
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-03 10:02:08 -0300

    add new storage keys

--- src/storage/storage.entities.ts
+++ src/storage/storage.entities.ts
@@ -1,1 +1,5 @@
 export const TIMES_DONE = "timesDone";
+export const CLOCK_KEY = "clockPoints";
+export const WORD_KEY = "wordPoints";
+export type  ClockPoints = 0 | 2;
+export const HISTORY = "history";
