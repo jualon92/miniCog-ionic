b90e3dfef3ee712995220078d2e7205d5fa93b60
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-03 00:54:57 -0300

    feat: implement dynamic word sets based on visit count in three-word registration

--- src/app/pages/three-word-registration/three-word-registration.page.ts
+++ src/app/pages/three-word-registration/three-word-registration.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/storage/storage.service';
 
 @Component({
   selector: 'app-three-word-registration',
   templateUrl: './three-word-registration.page.html',
   styleUrls: ['./three-word-registration.page.scss'],
+  providers: [StorageService],
 })
 export class ThreeWordRegistrationPage implements OnInit {
+  timesVisited: number = 0;
+  constructor(
+    private storage: StorageService,
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    //TODO: utilizar resolvers
+    setTimeout(async () => {
+      this.timesVisited = await this.storage.getTimesDone();
+   
+    }, 100);
+     
+    
   }
 
 }
