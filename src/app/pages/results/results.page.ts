15b96e773d182d353a0cde96089a421e7cc0b7d4
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-04 00:26:41 -0300

    update results page to display score details

--- src/app/pages/results/results.page.ts
+++ src/app/pages/results/results.page.ts
@@ -1,13 +1,26 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { StorageService } from 'src/storage/storage.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.page.html',
   styleUrls: ['./results.page.scss'],
+  providers: [StorageService],
 })
-export class ResultsPage {
+export class ResultsPage implements OnInit {
+  threeWordRecallPoints:any;
+  clockDrawingPoints: any;
+  total = 0;
 
-  constructor() { }
+  constructor(public storage: StorageService) { }
+
+  async ngOnInit() {
+      setTimeout(async () => {
+        this.threeWordRecallPoints = await  this.storage.getWordPoints();
+        this.clockDrawingPoints = await this.storage.getClockPoints() ?? 0; 
+      }, 100);
+  }
 
 
 
