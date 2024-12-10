ce4df72b2e98b19384f5e026f1a84c831a4631d1
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-10 20:45:45 -0300

    add read more component

---
+++ src/app/pages/shared/buttons/read-more/read-more.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+ 
+@Component({
+  selector: 'app-read-more',
+  templateUrl: './read-more.component.html',
+  styleUrls: ['./read-more.component.scss'],
+  standalone: true,
+  imports: [IonicModule]
+})
+export class ReadMoreComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
