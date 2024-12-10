7173167251687e9a7bee489a9a11a02bbe136054
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-10 20:48:10 -0300

    fix read more btn not displayed

--- src/app/pages/shared/buttons/read-more/read-more.component.ts
+++ src/app/pages/shared/buttons/read-more/read-more.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { TranslocoRootModule } from 'src/app/transloco-root.module';
  
 @Component({
   selector: 'app-read-more',
   templateUrl: './read-more.component.html',
   styleUrls: ['./read-more.component.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, TranslocoRootModule]
 })
 export class ReadMoreComponent  implements OnInit {
 
