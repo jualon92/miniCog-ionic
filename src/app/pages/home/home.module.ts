51d64929e57ca549e6245ab4996debe361cf2ebb
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-16 11:38:58 -0300

    add use mobile page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -20,14 +20,16 @@ import { ToastModule } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { ShowHistoryComponent } from '../shared/buttons/show-history/show-history.component';
-
-
+import { Card, CardModule } from 'primeng/card';
+import { QRCodeModule } from 'angularx-qrcode';
 @NgModule({
   imports: [
     DropdownModule,
     CommonModule,
     FormsModule,
+    CardModule,
     IonicStorageModule,
+    QRCodeModule,
     ToastModule,
     ButtonModule,
     TranslocoRootModule,
