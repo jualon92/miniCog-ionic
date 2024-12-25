34ba29b2cb267534e5d2e8adbe83be1833ef9ee6
Juan Ignacio Londra <londrajuan@gmail.com>
2024-12-25 03:16:56 -0300

    refactor home component

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -25,21 +25,12 @@ export class HomePage implements  AfterViewInit, OnInit {
   displayLangOptions = false;
   NO_LANG_CACHE = false; 
   isMobile = false;
-  isModalOpen = false;
-  comment = '';
-  feedback= "";
-  value= "";
   @ViewChild(IonModal) modal!: IonModal;
-
-  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
- 
    
 
   constructor(
-    private translocoService: TranslocoService,
     private storage: StorageService,
     private messageService: MessageService,
-    private homeService: HomeService,
   ) {}
 
   ngOnInit(): void {
@@ -53,48 +44,6 @@ export class HomePage implements  AfterViewInit, OnInit {
 
  
 
-  cancel() {
-    this.modal.dismiss(null, 'cancel');
-  }
-
-  learnMore(){
-    
-  }
-  confirm() { 
-    this.homeService.saveFeedback(this.feedback).pipe(catchError( (err) => this.showSendFeedbackNotification(err) )).subscribe(response => {
-      
-      this.messageService.add({ severity: 'success', summary: 'Feedback', detail: "comentario enviado" });
-      this.feedback = "";
-
-      
-      this.modal.dismiss(this.feedback, 'confirm');
-    });
-    
- 
-  }
-
-
-  showSendFeedbackNotification(err: any): Observable<any> {
-    const errorMessage = err.status === 503 
-        ? 'Has excedido el límite de intentos. Por favor, intenta mas tarde.' 
-        : 'Error al guardar el comentario';
-
-    this.messageService.add({
-        severity: 'error',
-        summary: 'Error',
-        detail: errorMessage
-    });
-
-    return of(err);
-}
-
- 
-  onWillDismiss(event: Event) {
-    const ev = event as CustomEvent<any>;
-    if (ev.detail.role === 'confirm') {
-      this.message = `Hello, ${ev.detail.data}!`;
-    }
-  }
 
  
   ngAfterViewInit() {
