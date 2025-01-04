0d6b42c8e3dba4673b02851e6a928db90d60f892
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-04 17:47:59 -0300

    read more should take into account lang

--- src/app/pages/shared/buttons/read-more/read-more.component.ts
+++ src/app/pages/shared/buttons/read-more/read-more.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { getBrowserLang } from '@jsverse/transloco';
 import { TranslocoRootModule } from 'src/app/transloco-root.module';
  
+export interface LANGURL{
+  en:string;
+  es:string;
+}
 @Component({
   selector: 'app-read-more',
   templateUrl: './read-more.component.html',
@@ -9,10 +14,24 @@ import { TranslocoRootModule } from 'src/app/transloco-root.module';
   standalone: true,
   imports: [IonicModule, TranslocoRootModule]
 })
-export class ReadMoreComponent {
-
+export class ReadMoreComponent implements OnInit {
+  pdfUrl = "";
   constructor() { }
 
+  private readonly  PDF_URLS : LANGURL  = {
+    'es': 'https://mini-cog.com/wp-content/uploads/2022/09/SPANISH-Mini-Cog.pdf',
+    'en': 'https://www.alz.org/media/documents/mini-cog.pdf'
+  } 
+
+  ngOnInit() {
+    //get lang code
+     const code = (getBrowserLang() ?? "en") as keyof LANGURL;
+ 
+    this.pdfUrl = this.PDF_URLS[code] 
+ 
+  }
+
+ 
 
 
 }
