ca0198482d74f11a808d8398b8d3edcce3bd7683
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-04 03:23:59 -0300

    add toolbar component, DRY

---
+++ src/app/pages/toolbar/toolbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, input, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon } from "@ionic/angular/standalone";
+import { LeaveACommentComponent } from '../shared/leave-a-comment/leave-a-comment.component';
+import { TranslocoModule } from '@jsverse/transloco';
+import { TranslocoRootModule } from 'src/app/transloco-root.module';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
+  standalone: true,
+  imports: [TranslocoRootModule, IonButtons, IonHeader, IonToolbar, IonIcon, IonTitle, IonButton, LeaveACommentComponent],
+  
+})
+export class ToolbarComponent  implements OnInit {
+  @Input() title: string = "";
+  modalId = "";
+  constructor() {
+    this.modalId = 'modal-' + Math.random().toString(36).substring
+   }
+
+  ngOnInit() {
+   
+  }
+
+}
