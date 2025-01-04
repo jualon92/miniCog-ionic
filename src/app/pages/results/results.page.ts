de31825a3b94d8b2368c496ca51955f6db665b13
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-04 04:52:59 -0300

    remove history button

--- src/app/pages/results/results.page.ts
+++ src/app/pages/results/results.page.ts
@@ -12,13 +12,15 @@ export class ResultsPage implements OnInit {
   threeWordRecallPoints : number = 0;
   clockDrawingPoints  : number  = 0;
   total  : number  = 0;
-
+  isSuccess = false;
+  successMessage = "Results saved in history"
   constructor(public storage: StorageService) { }
 
   async ngOnInit() {
       setTimeout(async () => {
         this.threeWordRecallPoints = await  this.storage.getWordPoints();
         this.clockDrawingPoints = await this.storage.getClockPoints() ?? 0; 
+        this.isSuccess = true;
       }, 1);
   }
 
