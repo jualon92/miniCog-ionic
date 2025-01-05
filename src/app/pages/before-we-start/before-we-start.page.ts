7bf6721efb399941e02fd9c04ede822419a345d8
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-05 17:42:40 -0300

    implement username history

--- src/app/pages/before-we-start/before-we-start.page.ts
+++ src/app/pages/before-we-start/before-we-start.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { AlertController, IonAlert } from '@ionic/angular';
+import { StorageService } from 'src/storage/storage.service';
+import { GetUsernameService } from '../dialogs/get-username.service';
 
 @Component({
   selector: 'app-before-we-start',
   templateUrl: './before-we-start.page.html',
   styleUrls: ['./before-we-start.page.scss'],
 })
-export class BeforeWeStartPage implements OnInit {
+export class BeforeWeStartPage {
 
-  constructor() { }
+  constructor(
+    private alertController: AlertController,
+    private storage: StorageService,
+    private getUsernameService: GetUsernameService,
+  ) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.initializeAlert();
+  }
+
+  async initializeAlert() {
+    const alert = await this.alertController.create(this.getUsernameService.getUsernameDialog());
+
+    await alert.present();
   }
 
 }
