8703c422cd59fee163808d42b3b70cae104afd91
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-06 19:44:57 -0300

    update toolbar to show tts state via toast

--- src/app/pages/toolbar/toolbar.component.ts
+++ src/app/pages/toolbar/toolbar.component.ts
@@ -1,6 +1,6 @@
 import { TextToSpeech } from '@capacitor-community/text-to-speech';
 import { Component, Input, input, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon } from "@ionic/angular/standalone";
+import { IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon, IonToast } from "@ionic/angular/standalone";
 import { LeaveACommentComponent } from '../shared/leave-a-comment/leave-a-comment.component';
 import { TranslocoModule } from '@jsverse/transloco';
 import { TranslocoRootModule } from 'src/app/transloco-root.module';
@@ -15,34 +15,41 @@ import { AsyncPipe } from '@angular/common';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
   standalone: true,
-  imports: [AsyncPipe,  TranslocoRootModule, IonButtons, IonHeader, IonToolbar, IonIcon, IonTitle, IonButton, LeaveACommentComponent],
-  
+  imports: [IonToast, AsyncPipe,  TranslocoRootModule, IonButtons, IonHeader, IonToolbar, IonIcon, IonTitle, IonButton, LeaveACommentComponent],
+
 })
 export class ToolbarComponent  implements OnInit {
   @Input() title: string = "";
   loudspeakerColor = "unset";
   loudspeakerIsActive$ = this.tts.enabled$;
-  timesVisited: any = 0; 
+  isTtsDisabled = false;
+  isTtsEnabled = false;
+  timesVisited: any = 0;
   constructor(public tts: TtsService, public storage: StorageService, public router : Router, public navController: NavController) {
- 
+
    }
 
   async ngOnInit() {
     setTimeout(async () => {
        const data =  await this.storage.getHistory();
        this.timesVisited =  data?.length;
-       
+
     }, 100);
-      
-   
-  } 
+
+
+  }
 
   useTts(){
     this.tts.trigger();
+    if (this.tts.getState()){
+      this.isTtsDisabled = true;
+    }else{
+      this.isTtsEnabled =  true;
+    }
    
-  
 
- 
+
+
   }
   goToHistory(){
     this.navController.navigateForward('history');
